e30fbc0000163c193a316aa0ff3734b60205aeb9
cesarecicolin <cece.caster@gmail.com>
2023-10-19 14:33:24 +0200

    add: racolta dati

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// DATI
+
+const userNameElem = document.getElementById("name");
+console.log(userNameElem)
+const km = parseInt(document.getElementById("km"));
+console.log(km)
+const userAgeElem = document.getElementById("age");
